7af437b8074c77dd122874d97e29acfa8b857ef7
Martin Gazzara <martingazzara@gmail.com>
2018-10-22 14:06:46 -0300

    Exercise III

--- 01-JS-DOM-APIs/scripts/topic1.js
+++ 01-JS-DOM-APIs/scripts/topic1.js
@@ -1,7 +1,19 @@
-function show(){
-    document.getElementById("show-this").className = "show-section";
+function tellMeAJoke(){
+  
+    fetch("http://api.icndb.com/jokes/random")
+        .then(response => response.json())
+        .then(data => data.value.joke)
+        .then(joke => {
+           
+            let place = document.getElementById("jokes");
+            let para = document.createElement("p");
+            let new_joke = document.createTextNode(joke);
+            
+            para.appendChild(new_joke); 
+            place.insertBefore(para,place.childNodes[0]);
+    });
+  
 }
 
-function saludate(){
-    alert('Hello, visitor');    
-}
+
+

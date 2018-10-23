9126de7dcac3f4e5e0b7a87dbc8efa12abf899ec
Martin Gazzara <martingazzara@gmail.com>
2018-10-23 19:52:09 -0300

    Exercise IV

--- 01-JS-DOM-APIs/scripts/topic1.js
+++ 01-JS-DOM-APIs/scripts/topic1.js
@@ -1,24 +1,3 @@
-/*
-
-function tellMeAJoke(){
-  
-    fetch("http://api.icndb.com/jokes/random")
-        .then(response => response.json())
-        .then(data => data.value.joke)
-        .then(joke => {
-           
-            let place = document.getElementById("jokes");
-            let para = document.createElement("p");
-            let new_joke = document.createTextNode(joke);
-            
-            para.appendChild(new_joke); 
-            place.insertBefore(para,place.childNodes[0]);
-    });
-  
-}
-
-*/
-
 function getInfo(config){
     const xhr = new XMLHttpRequest();
     
@@ -28,9 +7,9 @@ function getInfo(config){
     return new Promise((resolve,rejected) => 
         xhr.onreadystatechange = function(){
             if (this.readyState == 4 && this.status == 200){
-                resolve(this.responseText);
-            }else if(this.status == 404){
-                rejected("error");
+                resolve(this.getResponseHeader("type"));
+            }else if (this.status == 404){
+                rejected("An error occurred");
             }
         }
     );
@@ -43,6 +22,12 @@ config.mode = "get";
 config.url = "http://api.icndb.com/jokes/random";
 
 getInfo(config).then(resolve => console.log(resolve))
-                .catch(error => console.log(error));
+                .catch( error => {
+                    
+                    let sector = document.getElementById("section-container");
+                
+                    sector.style.backgroundColor = "#df0000";
 
+                    sector.innerHTML = "<p>" + error +"</p>";
 
+                });

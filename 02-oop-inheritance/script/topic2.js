ade056a2400a66ab83af15633fa4640034c56735
Martin Gazzara <martingazzara@gmail.com>
2018-10-27 14:48:24 -0300

    Exercise II

--- 02-oop-inheritance/script/topic2.js
+++ 02-oop-inheritance/script/topic2.js
@@ -1,31 +1,3 @@
-class Movie {
-
-    constructor(title, year, duration){
-        this.title = title;
-        this.year = year;
-        this.duration = duration;
-    }
-
-    play(){
-        console.log("Playing "+ this.title + ".");
-    }
-
-    pause(){
-        console.log(this.title + " has been paused.");
-    }
-
-    resume(){
-        console.log("You have resumed "+ this.title + ".");
-    }
-}
-
-class Actor{
-    constructor(name, age){
-        this.name = name;
-        this.age = age;
-    }
-}
-
 class EventEmitter{
 
     constructor(){
@@ -56,10 +28,51 @@ class EventEmitter{
     
 }
 
+class Movie extends EventEmitter {
+
+    constructor(title, year, duration){
+        super();
+        this.title = title;
+        this.year = year;
+        this.duration = duration;
+    }
+
+    play(){
+        super.emit("playing");
+    }
+
+    pause(){
+        super.emit("paused");
+    }
+
+    resume(){
+        super.emit("resumed");
+    }
+}
+
+class Actor{
+    constructor(name, age){
+        this.name = name;
+        this.age = age;
+    }
+}
+
+/* Movies */
+
 let troy = new Movie("Troy", 2004, 160);
+let LordRing = new Movie("The Lord of the Ring: The Fellowship of the Ring", 2001, 178);
+
+/* Events */
+troy.on("playing", () => console.log("Playing movie"));
+troy.on("paused", () => console.log("The movie has been paused"));
+troy.on("resumed", () => console.log("You have resumed the movie"));
+
+LordRing.on("playing", () => console.log("Playing movie"));
+LordRing.on("paused", () => console.log("The movie has been paused"));
+LordRing.on("resumed", () => console.log("You have resumed the movie"));
+
+/* Movies functions */
 
 troy.play();
-
 troy.pause();
-
 troy.resume();

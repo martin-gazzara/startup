1e68670db018285430739138f96244d86770050f
Martin Gazzara <martingazzara@gmail.com>
2018-11-05 22:15:57 -0300

    Exercise IV

--- 04-UI-ReactJS/topic4-app/src/Movie.js
+++ 04-UI-ReactJS/topic4-app/src/Movie.js
@@ -10,7 +10,7 @@ class Movie extends React.Component{
             <div className="MovieItem">
                 <h3>{this.props.name}</h3>
                 {this.props.EditMode ? <span></span> : <button onClick={this.props.EditName} value={this.props.Key}>Edit</button>}
-                
+                {this.props.EditMode ? <span></span> : <button onClick={this.props.Delete} value={this.props.Key}>Remove</button>}
             </div>
         )
     }
--- 04-UI-ReactJS/topic4-app/src/MovieList.js
+++ 04-UI-ReactJS/topic4-app/src/MovieList.js
@@ -17,6 +17,7 @@ class MovieList extends React.Component{
         this.AddMovie = this.AddMovie.bind(this);
         this.EditMovie = this.EditMovie.bind(this);
         this.ApplyChanges = this.ApplyChanges.bind(this);
+        this.DeleteMovie = this.DeleteMovie.bind(this);
     }
 
     HandleInput(e){
@@ -55,13 +56,25 @@ class MovieList extends React.Component{
         })
     }
 
+    DeleteMovie(e){
+        let movie = this.state.movieList[e.target.value];
+        let newMovieList = this.state.movieList.filter( mov => mov != movie);
+        this.setState({
+            movieList:newMovieList
+        })
+    }
+
     render(){
         return (
             <div className="MovieList">
                 <h3>Insert you favourite movie:</h3>
                 <input type="text" onChange={this.HandleInput} value={this.state.newInput} placeholder="Insert movie"/>
                 <button onClick={this.state.editMode ? this.ApplyChanges : this.AddMovie}>{this.state.editMode ? "Edit" : "Add"}</button>
-                {this.state.movieList.map( (movie,i) => <Movie EditMode={this.state.editMode} EditName={this.EditMovie} Key={i} name={movie}/>)}
+                {this.state.movieList.map( (movie,i) => <Movie 
+                    Delete={this.DeleteMovie}
+                    EditMode={this.state.editMode} 
+                    EditName={this.EditMovie} 
+                    Key={i} name={movie}/>)}
             </div>
         )
     }

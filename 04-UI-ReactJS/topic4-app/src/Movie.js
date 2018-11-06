49b9141e7e2e1811b164697b90312a413b3b6f65
Martin Gazzara <martingazzara@gmail.com>
2018-11-05 22:02:19 -0300

    Exercise III, the Exercise II was been commited in the previous commit

--- 04-UI-ReactJS/topic4-app/src/Movie.js
+++ 04-UI-ReactJS/topic4-app/src/Movie.js
@@ -9,6 +9,8 @@ class Movie extends React.Component{
         return (
             <div className="MovieItem">
                 <h3>{this.props.name}</h3>
+                {this.props.EditMode ? <span></span> : <button onClick={this.props.EditName} value={this.props.Key}>Edit</button>}
+                
             </div>
         )
     }
--- 04-UI-ReactJS/topic4-app/src/MovieList.js
+++ 04-UI-ReactJS/topic4-app/src/MovieList.js
@@ -8,11 +8,15 @@ class MovieList extends React.Component{
         super();
         this.state = {
             newInput:"",
-            movieList: []
+            movieList: [],
+            editMode:false,
+            itemToEdit:undefined
         }
 
         this.HandleInput = this.HandleInput.bind(this);
         this.AddMovie = this.AddMovie.bind(this);
+        this.EditMovie = this.EditMovie.bind(this);
+        this.ApplyChanges = this.ApplyChanges.bind(this);
     }
 
     HandleInput(e){
@@ -32,16 +36,37 @@ class MovieList extends React.Component{
         }
     }
 
+    EditMovie(e){
+        let index = e.target.value;
+        this.setState({newInput:this.state.movieList[index],editMode:true,itemToEdit:index});
+    }
+
+    ApplyChanges(){
+        let newValue = this.state.newInput;
+        let newMovieList = this.state.movieList;
+
+        newMovieList[this.state.itemToEdit] = newValue;
+
+        this.setState({
+            newInput:"",
+            movieList:newMovieList,
+            editMode:false,
+            itemToEdit:undefined
+        })
+    }
+
     render(){
         return (
             <div className="MovieList">
                 <h3>Insert you favourite movie:</h3>
                 <input type="text" onChange={this.HandleInput} value={this.state.newInput} placeholder="Insert movie"/>
-                <button onClick={this.AddMovie}>Add</button>
-                {this.state.movieList.map( movie => <Movie name={movie}/>)}
+                <button onClick={this.state.editMode ? this.ApplyChanges : this.AddMovie}>{this.state.editMode ? "Edit" : "Add"}</button>
+                {this.state.movieList.map( (movie,i) => <Movie EditMode={this.state.editMode} EditName={this.EditMovie} Key={i} name={movie}/>)}
             </div>
         )
     }
 }
+
+//
 
 export default MovieList;
